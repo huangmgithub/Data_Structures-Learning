4416adcc3ff0f8697e7fdb4fd92d7416940caf53
Black Rose <39798696+huangmgithub@users.noreply.github.com>
2019-06-22 23:06:22 +0800

    Modify base.py

--- Chapter06_Set_Map/base.py
+++ Chapter06_Set_Map/base.py
@@ -13,3 +13,24 @@ class SetBase:
 
     def is_Empty(self):
         raise NotImplementedError
+
+class MapBase:
+    def add(self, key, value):
+        raise NotImplementedError
+
+    def remove(self, key):
+        raise NotImplementedError
+
+    def contains(self, key):
+        raise NotImplementedError
+
+    def get(self, key):
+        raise NotImplementedError
+
+    def set(self, key, value):
+        raise NotImplementedError
+
+    def get_Size(self):
+        raise NotImplementedError
+
+        raise NotImplementedError
