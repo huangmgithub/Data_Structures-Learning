9febc993b5583f4114434cb396fe05e7ba7a6e31
Black Rose <39798696+huangmgithub@users.noreply.github.com>
2019-06-24 22:49:51 +0800

    Modify Priority_Queue code

--- Chapter07_MaxHeap/priority_queue.py
+++ Chapter07_MaxHeap/priority_queue.py
@@ -1,2 +1,21 @@
-class PriorityQueue:
-	pass
+from Chapter07_MaxHeap.max_heap import MaxHeap
+from Chapter03_Queue.base import Queuebase
+
+class PriorityQueue(Queuebase):
+    def __init__(self):
+        self._max_heap = MaxHeap()
+
+    def get_Size(self):
+        return self._max_heap.size()
+
+    def is_Empty(self):
+        return self._max_heap.is_Empty()
+
+    def get_Front(self):
+        return self._max_heap._find_max()
+
+    def enqueue(self, e):
+        self._max_heap.add(e)
+
+    def dequeue(self):
+        return self._max_heap._extract_max()
